d815273576606b1ecb3c181ed1d135045d3bbda1
Brian Campbell <Brian.Campbell@ed.ac.uk>
2018-11-26 16:34:49 +0000

    Match protocol with what I wrote for TestRIG

--- main.ml
+++ main.ml
@@ -21,65 +21,86 @@ let set_bytes bytes off v =
 
 let gens, socket, num =
   let gens = ref rand_gens in
-  let sock = ref false in
-  let host = ref "localhost" in
-  let port = ref "1234" in
+  let port = ref None in
   let num = ref 1 in
   let open Arg in
   let () = parse
     (align
-       ["-c", Set sock, " Connect to socket";
-        "-h", String (fun s -> host := s; sock := true), "<host> Host to connect to (default \"localhost\"), implies -c";
-        "-p", String (fun p -> port := p; sock := true), "<port> Port to connect to (default \"1234\"), implies -c";
+       ["-p", String (fun p -> port := Some p), "<port> Provide an instruction generator service on this port";
         "-no_compressed", Unit (fun () -> gens := remove_compressed !gens), " Generate non-compressed instructions";
         "-n", Set_int num, "<number> Number of instructions to generate";
         "-restrict_registers", Unit (fun () -> gens := restrict_registers !gens), " Apply a simple-minded restriction on the choice of registers"])
     (fun _ -> raise (Bad "Unexpected argument"))
     "RISC-V instruction generator"
   in !gens,
-     (if !sock then
+     (match !port with
+      | Some port ->
         let open Unix in
-        let addr::_ = getaddrinfo !host !port [AI_FAMILY PF_INET; AI_SOCKTYPE SOCK_STREAM] in
+        let addr::_ = getaddrinfo "localhost" port [AI_FAMILY PF_INET; AI_SOCKTYPE SOCK_STREAM] in
         let sock = socket ~cloexec:true PF_INET SOCK_STREAM 0 in
-        let () = connect sock addr.ai_addr in
+        let () = setsockopt sock SO_REUSEADDR true in
+        let () = bind sock addr.ai_addr in
+        let () = listen sock 1 in
         Some sock
-      else None),
+      | None -> None),
      !num
+;;
+
+let rec generate_one () =
+  (* Loop until we get an instruction supported by the encoder *)
+  let rec aux () =
+    try
+      let instr = gens.gen_zast gens in
+      let bits =
+        try zencdec_forwards instr
+        with Match_failure _ -> zencdec_compressed_forwards instr
+      in
+      let () = print_endline (string_of_bits bits ^ " " ^ zassembly_forwards instr) in
+      instr, bits
+    with Match_failure _ -> aux ()
+  in aux ()
 ;;
 
 let rec generate n =
   if n = 0 then () else
-  let instr, bits =
-    (* Loop until we get an instruction supported by the encoder *)
-    let rec aux () =
-      try
-        let instr = gens.gen_zast gens in
-        let bits =
-          try zencdec_forwards instr
-          with Match_failure _ -> zencdec_compressed_forwards instr
-        in
-        let () = print_endline (string_of_bits bits ^ " " ^ zassembly_forwards instr) in
-        instr, bits
-      with Match_failure _ -> aux ()
-    in aux ()
-  in
-  let () =
-    match socket with
-    | None -> ()
-    | Some sock ->
-       let bytes = Bytes.make 8 '\000' in
-       let () = Bytes.set bytes 4 '\001' in
-       let () = set_bytes bytes 0 bits in
-       if Unix.send sock bytes 0 8 [] <> 8
-       then prerr_endline "Unable to send full instruction"
-       else ()
-  in generate (n-1)
+  let _ = generate_one () in
+  generate (n-1)
 ;;
 
-Random.self_init ();;
-generate num;;
+let rand_init_bytes buf =
+  let rec aux i n =
+    let n = int_of_char (Bytes.get buf i) + n * 256 in
+    if i = 0 then n else aux (i-1) n
+  in Random.init (aux 3 0)
+
+let server sock =
+     let buf = Bytes.make 4 '\000' in
+     let rec aux () =
+       if Unix.recv sock buf 0 4 [] <> 4
+       then prerr_endline "Unable to read full seed"
+       else
+         let () = rand_init_bytes buf in
+         let instr, bits = generate_one () in
+(*         let bytes = Bytes.make 8 '\000' in
+         let () = Bytes.set bytes 4 '\001' in
+         let () = set_bytes bytes 0 bits in
+         if Unix.send sock bytes 0 8 [] <> 8*)
+         let bytes = Bytes.make 4 '\000' in
+         let () = set_bytes bytes 0 bits in
+         if Unix.send sock bytes 0 4 [] <> 4
+         then prerr_endline "Unable to send full instruction"
+         else aux ()
+     in aux ()
+;;
 
 match socket with
-| None -> ()
-| Some sock -> Unix.shutdown sock Unix.SHUTDOWN_ALL
+| None ->
+   let () = Random.self_init () in
+   generate num
+| Some sock ->
+   let () = print_endline "Waiting for connection..." in
+   let sock', _ = Unix.accept sock in
+   let () = server sock' in
+   Unix.shutdown sock' Unix.SHUTDOWN_ALL;
+   Unix.shutdown sock Unix.SHUTDOWN_ALL
 ;;
